440e28f09f4ffaf71443e43cc715a1aab3b898fa
deanorhan <deanorhan@gmail.com>
2022-08-11 16:57:55 -0700

    added the pagination and health check endpoint

--- src/main/java/org/daemio/merch/service/MerchService.java
+++ src/main/java/org/daemio/merch/service/MerchService.java
@@ -2,7 +2,6 @@ package org.daemio.merch.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.daemio.merch.dto.MerchPage;
 import org.daemio.merch.error.MerchNotFoundException;
