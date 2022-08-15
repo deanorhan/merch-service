440d4e757f9949d9f6a8da34e63f09074c9c6b71
deanorhan <deanorhan@gmail.com>
2022-08-14 19:31:32 -0700

    adding in thumbnail images and buildship crap

---
+++ src/test/java/org/daemio/merch/repository/MerchRepositoryTest.java
@@ -0,0 +1,19 @@
+package org.daemio.merch.repository;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class MerchRepositoryTest {
+
+    @Autowired
+    private MerchRepository repo;
+    
+    @Test
+    public void checkAllIsWell() {
+        assertNotNull(repo, "Repository was not injected");
+    }
+}
